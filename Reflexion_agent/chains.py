fa7cfaf34137ee0b63d8c378d3f63db631db3152
Adarsha2004 <adarshanatia@gmail.com>
2025-06-13 08:54:54 +0530

    Graph of Reflexion

--- Reflexion_agent/chains.py
+++ Reflexion_agent/chains.py
@@ -35,10 +35,27 @@ first_responder_prompt_template = actor_prompt_template.partial(
 
 llm = ChatOpenAI(model="gpt-4o")
 
-first_responder_chain = first_responder_prompt_template | llm.bind_tools(tools=[AnswerQuestion], tool_choice='AnswerQuestion') | pydantic_parser
+first_responder_chain = first_responder_prompt_template | llm.bind_tools(tools=[AnswerQuestion], tool_choice='AnswerQuestion') 
 
-response = first_responder_chain.invoke({
-    "messages": [HumanMessage("Write a blog post on how samll business can leverage AI to grow")]
-})
+validator = PydanticToolsParser(tools=[AnswerQuestion])
 
-print(response)
+# Revisor section
+
+revise_instructions = """Revise your previous answer using the new information.
+    - You should use the previous critique to add important information to your answer.
+        - You MUST include numerical citations in your revised answer to ensure it can be verified.
+        - Add a "References" section to the bottom of your answer (which does not count towards the word limit). In form of:
+            - [1] https://example.com
+            - [2] https://example.com
+    - You should use the previous critique to remove superfluous information from your answer and make SURE it is not more than 250 words.
+"""
+
+revisor_chain = actor_prompt_template.partial(
+    first_instruction=revise_instructions
+) | llm.bind_tools(tools=[ReviseAnswer], tool_choice="ReviseAnswer")
+
+# response = first_responder_chain.invoke({
+#     "messages": [HumanMessage("AI Agents taking over content creation")]
+# })s
+
+# print(response)
